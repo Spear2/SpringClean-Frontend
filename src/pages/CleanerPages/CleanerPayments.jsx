2c9a01138b9938530ba9e5c4e14f9caa448715df
smokusjorkus <johannelumauag11@gmail.com>
2025-11-30 08:43:13 +0800

    added new routes for cleaner

---
+++ src/pages/CleanerPages/CleanerPayments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CleanerPayments() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/pages/CleanerPages/CleanerSchedule.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CleanerSchedule() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
