c22b33178eb35a4e18bcded2760422040638bdc8
josepharrybutihen <josepharrybutihen@gmail.com>
2025-12-06 15:09:49 +0800

    I added backend for company bookings page and some modifications for company pages js and css.

--- src/pages/CompanyPages/CompanyDashboard.js
+++ src/pages/CompanyPages/CompanyDashboard.js
@@ -38,8 +38,8 @@ export default function CleanersDashboard() {
     <div>
       <NavbarCleaner />
 
-      <div className="dashboard-container">
-        <header className="dashboard-header">
+      <div className="company-dashboard-container">
+        <header className="company-dashboard-header">
           <h1>
             Welcome Back, <span className="highlight">Cleaner!</span>
           </h1>
@@ -48,28 +48,28 @@ export default function CleanersDashboard() {
           </p>
         </header>
 
-        <div className="dashboard-cards">
-          <div className="dashboard-card">
+        <div className="company-dashboard-cards">
+          <div className="company-dashboard-card">
             <h3>Total Bookings</h3>
             <p className="number">12</p>
             <p className="desc">Bookings assigned this month</p>
           </div>
 
-          <div className="dashboard-card">
+          <div className="company-dashboard-card">
             <h3>Upcoming Schedules</h3>
             <p className="number">3</p>
             <p className="desc">Scheduled cleanings this week</p>
           </div>
 
-          <div className="dashboard-card">
+          <div className="company-dashboard-card">
             <h3>Payments Received</h3>
             <p className="number">₱8,500</p>
             <p className="desc">Total income this month</p>
           </div>
         </div>
 
-        <div className="recent-activity">
-          <h1 className="dashboard-title">Recent Bookings</h1>
+        <div className="company-recent-activity">
+          <h1 className="company-dashboard-title">Recent Bookings</h1>
           <table>
             <thead>
               <tr>
